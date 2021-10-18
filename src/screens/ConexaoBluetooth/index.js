71469f554a0282e1bae52f066ee6d1844d1abd72
Gustavo Antonio <46870937+Gustavo-Antonio@users.noreply.github.com>
2021-10-18 05:51:34 -0300

    Update index.js

--- src/screens/ConexaoBluetooth/index.js
+++ src/screens/ConexaoBluetooth/index.js
@@ -1,101 +1,260 @@
 import React, {useEffect} from "react";
-import {Platform,ScrollView,Switch,Text,SafeAreaView,View,ActivityIndicator,Modal,BackHandler,Linking} from "react-native";
+import {
+  Platform,
+  ScrollView,
+  Switch,
+  Text,
+  SafeAreaView,
+  View,
+  ActivityIndicator,
+  Modal,
+  BackHandler,
+
+} from "react-native";
+
 import Toast from "@remobile/react-native-toast";
-import Bluetooth, { Device } from 'react-native-ble-plx';
 import BluetoothSerial, {withSubscription} from "react-native-bluetooth-serial-next";
 import {Buffer} from "buffer";
-import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+
 import Button from "../../components/ButtonBluetooth";
 import DeviceList from "../../components/DeviceList";
 import styles from "./styles";
-import BleManager from 'react-native-ble-manager';
+
 global.Buffer = Buffer;
 
 const iconv = require("iconv-lite");
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.events = null;
     this.state = {
       isEnabled: false,
-      isScanning: false,
       device: null,
       devices: [],
       scanning: false,
       processing: false,
       navigator: props.navigator
     };
-
-
-    ReactNativeForegroundService.register();
-    ReactNativeForegroundService.add_task(() => {
-
-
-     BluetoothSerial.available().then((length) => {
-      BluetoothSerial.readFromDevice().then((data) => {
-        if(String(data)=="1"){
-          Toast.showShortBottom("Socorro Tipo 1");
-        }
-        if(String(data)=="2"){
-          Toast.showShortBottom("Socorro Tipo 2");
-        }
-      })
-    });
-
-   }, {
-    delay: 100,
-    onLoop: true,
-    taskId: "taskid",
-    onError: (e) => console.log(`Error logging:`, e),
-  });
   }
 
+  
   async componentDidMount() {
     this.events = this.props.events;
-    ReactNativeForegroundService.start({
-      id: 144,
-      title: "Hera",
-      message: "Você está protegida!",
-      vibration: true,
-      smallIcon: "ic_notification",
-      icon: "ic_notification",
-      largeicon: "ic_notification",
-      importance: "high",
-    });
 
-     BleManager.start({ showAlert: false })
-      .then(() => {
-        BleManager.enableBluetooth();
-        BleManager.connect('3C:61:05:14:DC:B6').then(() => {
-          console.log('Conectado');
-      }
-      ).catch((error) => {
-          console.log(error);
-      }
-      )
-
-  }).catch((error) => {
-    console.log(error)
-});
-    
     try {
-      const [devices] = await Promise.all([
-        BluetoothSerial.list()]
-        );
+      const [isEnabled, devices] = await Promise.all([
+        BluetoothSerial.isEnabled(),
+        BluetoothSerial.list()
+        ]);
+
       this.setState({
+        isEnabled,
         devices: devices.map(device => ({
           ...device,
           paired: true,
-          connected: false,
-        })),
+          connected: false
+        }))
       });
-
-      
     } catch (e) {
       Toast.showShortBottom(e.message);
     }
+
+
   }
 
+  async comunicacao() {
+    await BluetoothSerial
+    .available()
+    .then((length) => {
+      BluetoothSerial
+      .readFromDevice()
+      .then((data) => {
+        if(data =='1'){
+          Toast.showShortBottom("Socorro Tipo 1");
+        }
+        if(data =='2'){
+          Toast.showShortBottom("Socorro Tipo 2");
+        }
+     })
+    });
+  }
+  
+  requestEnable = () => async () => {
+    try {
+      await BluetoothSerial.requestEnable();
+      this.setState({ isEnabled: true });
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+    }
+  };
+  
+  toggleBluetooth = async value => {
+    try {
+      if (value) {
+        await BluetoothSerial.enable();
+      } else {
+        await BluetoothSerial.disable();
+      }
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+    }
+  };
+  
+  listDevices = async () => {
+    try {
+      const list = await BluetoothSerial.list();
+
+      this.setState(({ devices }) => ({
+        devices: devices.map(device => {
+          const found = list.find(v => v.id === device.id);
+
+          if (found) {
+            return {
+              ...found,
+              paired: true,
+              connected: false,
+
+            };
+          }
+
+          return device;
+        })
+      }));
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+    }
+  };
+  
+  discoverUnpairedDevices = async () => {
+    this.setState({ scanning: true });
+    console.log("Discovering unpaired devices...");
+
+    try {
+      console.log("Discovering  devices...");
+      const unpairedDevices = await BluetoothSerial.listUnpaired();
+
+      this.setState(({ devices }) => ({
+        scanning: false,
+        devices: devices
+        .map(device => {
+          const found = unpairedDevices.find(d => d.id === device.id);
+          console.log("Discovering...");
+          if (found) {
+            return {
+              ...device,
+              ...found,
+              connected: false,
+              paired: false
+            };
+          }
+
+          return device.paired || device.connected ? device : null;
+        })
+        .map(v => v)
+      }));
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+
+      this.setState(({ devices }) => ({
+        scanning: false,
+        devices: devices.filter(device => device.paired || device.connected)
+      }));
+    }
+  };
+  
+
+  
+  toggleDevicePairing = async ({ id, paired }) => {
+    if (paired) {
+      await this.unpairDevice(id);
+    } else {
+      await this.pairDevice(id);
+    }
+  };
+  
+  pairDevice = async id => {
+    this.setState({ processing: true });
+
+    try {
+      const paired = await BluetoothSerial.pairDevice(id);
+
+      if (paired) {
+        Toast.showShortBottom(
+          `Device ${paired.name}<${paired.id}> paired successfully`
+          );
+
+        this.setState(({ devices, device }) => ({
+          processing: false,
+          device: {
+            ...device,
+            ...paired,
+            paired: true
+          },
+          devices: devices.map(v => {
+            if (v.id === paired.id) {
+              return {
+                ...v,
+                ...paired,
+                paired: true
+              };
+            }
+
+            return v;
+          })
+        }));
+      } else {
+        Toast.showShortBottom(`Device <${id}> pairing failed`);
+        this.setState({ processing: false });
+      }
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+      this.setState({ processing: false });
+    }
+  };
+  
+  unpairDevice = async id => {
+    this.setState({ processing: true });
+
+    try {
+      const unpaired = await BluetoothSerial.unpairDevice(id);
+
+      if (unpaired) {
+        Toast.showShortBottom(
+          `Device ${unpaired.name}<${unpaired.id}> unpaired successfully`
+          );
+
+        this.setState(({ devices, device }) => ({
+          processing: false,
+          device: {
+            ...device,
+            ...unpaired,
+            connected: false,
+            paired: false
+          },
+          devices: devices.map(v => {
+            if (v.id === unpaired.id) {
+              return {
+                ...v,
+                ...unpaired,
+                connected: false,
+                paired: false
+              };
+            }
+
+            return v;
+          })
+        }));
+      } else {
+        Toast.showShortBottom(`Device <${id}> unpairing failed`);
+        this.setState({ processing: false });
+      }
+    } catch (e) {
+      Toast.showShortBottom(e.message);
+      this.setState({ processing: false });
+    }
+  };
+  
   toggleDeviceConnection = async ({ id, connected }) => {
     if (connected) {
       await this.disconnect(id);
@@ -103,13 +262,17 @@ class App extends React.Component {
       await this.connect(id);
     }
   };
-
+  
   connect = async id => {
     this.setState({ processing: true });
+
     try {
       const connected = await BluetoothSerial.device(id).connect();
 
       if (connected) {
+        Toast.showShortBottom(
+          `Connected to device ${connected.name}<${connected.id}>`
+          );
 
         this.setState(({ devices, device }) => ({
           processing: false,
@@ -130,16 +293,19 @@ class App extends React.Component {
             return v;
           })
         }));
-      } 
+      } else {
+        Toast.showShortBottom(`Failed to connect to device <${id}>`);
+        this.setState({ processing: false });
+      }
     } catch (e) {
       Toast.showShortBottom(e.message);
       this.setState({ processing: false });
     }
   };
-
-
+  
   disconnect = async id => {
     this.setState({ processing: true });
+
     try {
       await BluetoothSerial.device(id).disconnect();
 
@@ -165,10 +331,13 @@ class App extends React.Component {
       this.setState({ processing: false });
     }
   };
+  
 
   renderModal = (device, processing) => {
     if (!device) return null;
+
     const { id, name, paired, connected } = device;
+
     return (
       <Modal
       animationType="fade"
@@ -195,8 +364,6 @@ class App extends React.Component {
 
           {!processing && (
             <View style={{ marginTop: 20, width: "50%" }}>
-
-
             <Button
             title={connected ? "Desconectar" : "Conectar"}
             style={{
@@ -205,16 +372,29 @@ class App extends React.Component {
             textStyle={{ color: "#fff" }}
             onPress={() => this.toggleDeviceConnection(device)}
             />
+            {connected && (
+              <React.Fragment>
+              <Button
+              title="Alertar"
+              style={{
+                backgroundColor: "#22509d"
+              }}
+              textStyle={{ color: "#fff" }}
+              onPress={() =>
+               this.comunicacao()
+             }
+             />
 
-            <Button
-            title="Fechar"
-            onPress={() => this.setState({ device: null })}
-            />
-
-            </View>
-            )}
-            </View>
-            ) : null}
+             </React.Fragment>
+             )}
+             <Button
+             title="Fechar"
+             onPress={() => this.setState({ device: null })}
+             />
+             </View>
+             )}
+             </View>
+             ) : null}
       </Modal>
       );
     };
@@ -253,8 +433,10 @@ class App extends React.Component {
         </React.Fragment>
         )}
 
+
         </SafeAreaView>
         );
       }
     }
+
     export default withSubscription({subscriptionName: "events"})(App);

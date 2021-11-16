aec365f6d9b9dd2b072db4e672a2d36b589ba0fa
Leandro Xavier <leandro.heitor@econverse.com.br>
2021-11-15 22:12:58 -0300

    a

--- src/screens/Mapa/index.js
+++ src/screens/Mapa/index.js
@@ -1,26 +1,26 @@
-import React, { Component, useEffect, useState } from 'react';
-import { AppRegistry, Dimensions, StyleSheet, Text, View, Image } from 'react-native';
+import React, {Component, useEffect, useState} from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
 import MapView from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
-import { useAnimatedGestureHandler } from 'react-native-reanimated';
-import { Marker } from 'react-native-svg';
+import {useAnimatedGestureHandler} from 'react-native-reanimated';
+import {Marker} from 'react-native-svg';
 import TabNavigator from '../../components/TabNavigator';
 import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore'
-
+import firestore from '@react-native-firebase/firestore';
 
 export default function App() {
-
-  const [latitude,
-    setLatitude] = React.useState(25);
-  const [longitude,
-    setLongitude] = React.useState(12);
+  const [latitude, setLatitude] = React.useState(25);
+  const [longitude, setLongitude] = React.useState(12);
 
   const [nomeUsuaria, setNomeUsuaria] = useState('');
-
-
-
 
   const [position, setPosition] = useState({
     latitude: -23.5489,
@@ -31,48 +31,40 @@ export default function App() {
 
   var lat = 0;
   var long = 0;
-  useEffect(
-    () => {
-  
+  useEffect(() => {
     (async () => {
-
-    const user = auth().currentUser;
-    const userJSON = user.toJSON();
-    (await firestore().collection('Usuarias').get()).forEach(doc => {
-      if (doc.data().email == userJSON.email) {
-        setNomeUsuaria(doc.data().nome);
-
-      }
-
-    });
-
-   
-  })();
-
-    Geolocation.watchPosition((pos) => {
-      console.log(pos);
-      setPosition({
-        ...position,
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude,
-
+      const user = auth().currentUser;
+      const userJSON = user.toJSON();
+      (await firestore().collection('Usuarias').get()).forEach(doc => {
+        if (doc.data().email == userJSON.email) {
+          setNomeUsuaria(doc.data().nome);
+        }
       });
+    })();
 
-    }, (error) => {
-      console.log(error);
-    }, {
-      enableHighAccuracy: true,
-      accuracy:10,
-      timeout: 20000,
-      maximumAge: 1000,
-
-    });
-  },[null]);
- 
+    Geolocation.watchPosition(
+      pos => {
+        console.log(pos);
+        setPosition({
+          ...position,
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        accuracy: 10,
+        timeout: 20000,
+        maximumAge: 1000,
+      },
+    );
+  }, [null]);
 
   return (
     <View style={styles.container}>
-     
       <MapView
         style={styles.map}
         region={position}
@@ -81,52 +73,48 @@ export default function App() {
             ...position,
             latitude: e.nativeEvent.coordinate.latitude,
             longitude: e.nativeEvent.coordinate.longitude,
-          })}>
-
+          })
+        }>
         <MapView.Marker
           coordinate={{
             latitude: position.latitude,
             longitude: position.longitude,
           }}
-
           pinColor={'#e0195c'}
-        >
-          <View style={{width: 200, height: 100, position: 'absolute'}}>
-            <View style={{ width: 200, height: 32, backgroundColor: "#e0195c", borderRadius: 30 }}>
-              <Text style={{ color: "#fff", textAlign: "center", fontSize: 12 }}>Localização da usuária: {nomeUsuaria}</Text>
-            </View>
+          title={nomeUsuaria}
+          description={'Você está aqui'}
+          >
 
-            <View style={{ width: 200, display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <Image source={require('../../../assets/map-marker.png')} />
-            </View>
-          </View>
+
+          <Image
+            style={{width: 40, height: 40, resizeMode: 'contain', margin: 5}}
+            source={require('../../../assets/map-marker-icon.png')}
+          />
+
 
 
         </MapView.Marker>
-
       </MapView>
-          <TabNavigator tela="mapa" />
-     </View>
+      <TabNavigator tela="mapa" />
+    </View>
   );
-
 }
 const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
-    height: Dimensions.get("window").height,
-    width: Dimensions.get("window").width,
-    
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
   },
 
   container: {
     display: 'flex',
-    
-    height: Dimensions.get("window").height,
+
+    height: Dimensions.get('window').height,
     flexDirection: 'column-reverse',
     backgroundColor: '#fff',
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
     justifyContent: 'space-between',
-    fontFamily: "Bahnscrift",
+    fontFamily: 'Bahnscrift',
   },
 });

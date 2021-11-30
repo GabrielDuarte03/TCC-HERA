a9191b6f6f3026814f144bec3fc2430daadcefd1
Paulo <54506590+pauloRoberto07@users.noreply.github.com>
2021-11-30 07:50:22 -0300

    Create logoutRestrito.js

---
+++ ADM/js/logoutRestrito.js
@@ -0,0 +1,9 @@
+function logoff () {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        window.location.href = "../../index.html";
+    }).catch(function (error) {
+        // An error happened.
+    });
+
+}
